332888f21685ddf182d1638201ed7419f064705e
SplatTab <81938880+SplatTab@users.noreply.github.com>
2022-01-15 18:20:37 -0600

    Add files via upload

---
+++ RaLogger/Commands/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using Player = Exiled.Events.Handlers.Player;
+using Server = Exiled.Events.Handlers.Server;
+
+namespace Commands
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Singleton;
+        public static EventHandlers EventHandlers;
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            EventHandlers = new EventHandlers(this);
+            Player.Banning += EventHandlers.BanEvent;
+            Player.Kicking += EventHandlers.KickEvent;
+            Player.ChangingRole += EventHandlers.ForceChangeClass;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Player.Banning -= EventHandlers.BanEvent;
+            Player.Kicking -= EventHandlers.KickEvent;
+            Player.ChangingRole -= EventHandlers.ForceChangeClass;
+            Singleton = null;
+            EventHandlers = null;
+            base.OnDisabled();
+        }
+    }
+}
