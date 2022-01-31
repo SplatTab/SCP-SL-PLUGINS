8aac84b54c60f0836f59babb89001272549a97b4
SplatTab <81938880+SplatTab@users.noreply.github.com>
2022-01-31 04:32:10 +0000

    Add files via upload

---
+++ NotArchived/SCALELMAOOOOOO/README.md
@@ -0,0 +1,8 @@
+# SCALELMAOOOOOO
+scale lmaoooooo oh god no note: dont resize the dog too much
+
+# ok ok usage
+.setscale (playerid) (x) (y) (z)
+
+needed permissions:
+Player Management
---
+++ NotArchived/SCALELMAOOOOOO/src/ScaleLmao/Config.cs
@@ -0,0 +1,13 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ScaleLmao
+{
+    public sealed class Config : IConfig
+    {
+        [Description("Whether or not the plugin is enabled on this server.")]
+        public bool IsEnabled { get; set; } = true;
+    }
+}
